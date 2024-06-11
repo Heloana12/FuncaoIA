4e634e85ac3b6e3df88817098ecd6d4c3beb9b46
Heloana12 <129884527+Heloana12@users.noreply.github.com>
2024-06-11 15:26:54 -0300

    s

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
+const caixaAlternativas = document.querySelector(".caixa-aternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const caixatextoResultado = document.querySelector(".caixa-textoresultado");
+
+const perguntas = [
+    {
+    enunciado: "Pergunta 1", 
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+},
+{
+    enunciado: "Pergunta 1", 
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+}
+];
